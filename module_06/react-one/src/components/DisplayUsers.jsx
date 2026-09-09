9ded7c15be594230da1038aa453c27adfd2d3406
Mir Md Mabrur <96931879+mabrur007@users.noreply.github.com>
2026-09-10 00:46:50 +0600

    fetch api applied

--- module_06/react-one/src/components/DisplayUsers.jsx
+++ module_06/react-one/src/components/DisplayUsers.jsx
@@ -3,23 +3,58 @@ import { useEffect, useState } from "react";
 export default function DisplayUsers() {
 	const [data, setData] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState("");
+
+	// old method of data fetching
+	// useEffect(() => {
+	// 	fetch("https://jsonplaceholder.typicode.com/users")
+	// 		.then((res) => {
+	// 			if (!res.ok) {
+	// 				throw new Error(res.message || "Something went wrong");
+	// 			}
+	// 			return res.json();
+	// 		})
+	// 		.then((data) => setData(data))
+	// 		.catch((err) => setError(err))
+	// 		.finally(() => setIsLoading(false));
+	// }, []);
+
+	// modern method of data fetching : recommended*
 	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then((res) => res.json())
-			.then((data) => setData(data))
-			.finally(() => setIsLoading(false));
+		const fetchUsers = async () => {
+			try {
+				const res = await fetch("https://jsonplaceholder.typicode.com/user");
+				if (!res.ok) {
+					throw new Error(res.ok || "Something went wrong!!");
+				}
+				const data = await res.json();
+				setData(data);
+			} catch (err) {
+				setError(err.message);
+				console.log(err.message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		fetchUsers();
 	}, []);
 	console.log(data);
-
 	if (isLoading) {
 		return <p>loading....</p>;
 	}
 
 	return (
 		<div>
+			<h2>{error}</h2>
 			{data.map((d) => (
 				<p key={d.id}>{d.name}</p>
 			))}
 		</div>
 	);
 }
+// useEffect(() => {
+// 		fetch("https://jsonplaceholder.typicode.com/users")
+// 			.then((res) => res.json())
+// 			.then((data) => setData(data))
+// 			.finally(() => setIsLoading(false));
+// 	}, []);
